fecdb691cc073c7bf802a54bfe1b714118bdc29c
twanle <86743134+twanle@users.noreply.github.com>
2021-07-01 02:36:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Bé iu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Yêu em yêu em yêu những giấc mơ
+    Giấc mơ dài thật dài mà anh k có lối ra
+    Yêu em yêu em yêu chỉ em thui
+    Yêu em hết thân này, yêu em chẳng tiếc gì`,
     btnIntro: 'Happy 400 days together',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Anh chồng ưng nhất quả đất <33',
     btnNo: 'Không, chán anh lắm :(',
     question:'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
-    btnReply: 'Nói chồng nghe <3',
+    btnReply: 'Nói chồng nghe coi <3',
     reply: 'Vì anh chồng là người thương em nhất quả đất <33333333',
     mess: 'Ỏ, bé đáng iu thía 🥰. Yêu bé nhắm nhắm 😘😘',
     messDesc: 'Nay mình phải celebrate thật to nha công chúa.',
