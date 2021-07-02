b41604e66cfd3811dc0a30b91977b76eacb78146
twanle <86743134+twanle@users.noreply.github.com>
2021-07-03 04:55:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: 'You are the best present I have ever had'
                 'You are the most beautyfull girl I have ever seen' 
                 'You are the sexiest lady I have ever sex'
-               'You are my everthing :3',
+               'You are my everthing :3 ',
     btnIntro: 'Happy 400 days together',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
